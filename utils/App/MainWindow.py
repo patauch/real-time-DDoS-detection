a9c704566db2e7a052e99c4a2869f7a849e3a16c
patauch <denchicl123@gmail.com>
2024-04-14 13:27:56 +0500

    chore: update template for header in app

--- utils/App/MainWindow.py
+++ utils/App/MainWindow.py
@@ -1,16 +1,36 @@
 from PyQt6.QtCore import Qt
-from PyQt6.QtWidgets import QMainWindow, QPushButton, QWidget
+from PyQt6.QtWidgets import (
+    QMainWindow, QPushButton,
+    QWidget, QLabel,
+    QLineEdit, QVBoxLayout,
+    QHBoxLayout, QComboBox)
 
 class MainWindow(QMainWindow):
     def __init__(self) -> None:
         super().__init__()
         self.setWindowTitle("DDoS Detection")
-        
+
         self.runButton = QPushButton("Run Detection")
-        self.runButton.setCheckable(True)
         self.runButton.clicked.connect(self.runButton_was_clicked)
 
-        self.setCentralWidget(self.runButton)
+        self.modelComboBox = QComboBox()
+
+        self.interfaceComboBox = QComboBox()
+
+        self.mainLayout = QVBoxLayout()
+
+        self.headerLayout = QHBoxLayout()
+        self.headerLayout.addWidget(self.runButton)
+        self.headerLayout.addWidget(self.modelComboBox)
+        self.headerLayout.addWidget(self.interfaceComboBox)
+
+
+        self.mainLayout.addLayout(self.headerLayout)
+
+        self.container = QWidget()
+        self.container.setLayout(self.mainLayout)
+
+        self.setCentralWidget(self.container)
 
     def runButton_was_clicked(self):
         print("Run Button Clicked!")
