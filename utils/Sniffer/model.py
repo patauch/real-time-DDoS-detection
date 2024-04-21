ada862980672e7bd34dd3020e733fe7fa3d7f1c6
patauch <denchicl123@gmail.com>
2024-04-21 19:11:04 +0500

    chore: update sniffer

---
+++ utils/Sniffer/model.py
@@ -0,0 +1,8 @@
+class Model():
+    def __init__(self, model_path, model_name) -> None:
+        self.model_path = model_path
+        self.model_name = model_name
+
+    def predict(self, features):
+        """Here should be written pattern facade"""
+        pass
